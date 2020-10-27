72def44d98d5238c31085842d35c63fdbf804b92
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-27 11:37:10 -0400

    fixed title in Post.jsx to show title

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -8,7 +8,7 @@ const Post = (props) => {
       <Link className="post" to={`/posts/${props._id}`}>
         <img className="post-image" src={props.imgURL} alt={props.name} />
         <div className="post-name">{props.name}</div>
-        <div className="price">{`${props.author}`}</div>
+        <div className="price">{`${props.title}`}</div>
       </Link>
     </>
   );
